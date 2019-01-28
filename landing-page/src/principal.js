98f6ccc2d75b464bffe838f5b0596b00486a51bc
Henrique Benak <henrique.benak@cea.com.br>
2019-01-27 23:54:18 -0300

    Configuracao de ambiente

---
+++ landing-page/src/principal.js
@@ -0,0 +1,8 @@
+// import Pessoa from './pessoa'
+// import './modules/moduloA'
+// import './assets'
+
+// const atendente = new Pessoa
+// console.log(atendente.cumprimentar())
+
+console.log('Config basic!');
