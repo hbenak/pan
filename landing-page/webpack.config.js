162fafa47543828e43dfea1dff3f569cc194fcca
Henrique Benak <henrique.benak@cea.com.br>
2019-01-30 09:38:38 -0300

    Configuracao de ambiente

--- landing-page/webpack.config.js
+++ landing-page/webpack.config.js
@@ -3,45 +3,53 @@ const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-	mode: modoDev ? 'development' : 'production',
-	entry: './src/principal.js',
-	output: {
-		filename: 'principal.js',
-		path: __dirname + '/public'
-	},
-	devServer: {
-		contentBase: './public',
-		port: 8080
-
-	},
-	optimization: {
-		minimizer: [
-			new UglifyJsPlugin({
-				cache: true,
-				parallel: true
-			}),
-			new OptimizeCSSAssetsPlugin({})
-		]
-	},
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: "estilo.css"
-		})
-	],
-	module: {
-		rules: [{
-			test: /\.s?[ac]ss$/,
-			use: [
-				MiniCssExtractPlugin.loader,
-				//'style-loader', // Adiciona CSS a DOM na tag <style>
-				'css-loader', // interpleta @import, url() ...
-				'sass-loader'
-			]
-		}, {
-			test: /\.(png|svg|jpg|gif)$/,
-			use: ['file-loader']
-		}]
-	}
+  mode: modoDev ? 'development' : 'production',
+  entry: './src/principal.js',
+  output: {
+    filename: 'principal.js',
+    path: __dirname + '/public'
+  },
+  devServer: {
+    contentBase: './public',
+    port: 8080
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "estilo.css"
+    }),
+    new CopyWebpackPlugin([
+      { context: 'src/', from: '**/*.html' },
+      { context: 'src/', from: 'imgs/**/*' }
+    ])
+  ],
+  module: {
+    rules: [{
+      test: /\.s?[ac]ss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        //'style-loader', // Adiciona CSS a DOM na tag <style>
+        'css-loader', // interpleta @import, url() ...
+        'sass-loader'
+      ]
+    }, {
+      test: /\.(png|svg|jpg|gif)$/,
+      use: ['file-loader']
+    }, {
+      test: /.(ttf|otf|eot|svg|woff(2)?)$/,
+      use: ['file-loader']
+    }]
+  }
 }
